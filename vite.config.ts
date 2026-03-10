91f6628e8d983e7c7d9a68877262a4e59c03016a
tejasvinu <tejasvinu@gmail.com>
2026-03-10 21:58:51 +0530

    feat: Add iOS native splash screens and maskable icons for an enhanced PWA experience.

--- vite.config.ts
+++ vite.config.ts
@@ -11,14 +11,14 @@ export default defineConfig({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,jpg}']
       },
       manifest: {
         name: 'Recomp 88',
         short_name: 'Recomp',
         description: 'Workout Tracker',
-        theme_color: '#0e0e0e',
-        background_color: '#0e0e0e',
+        theme_color: '#080808',
+        background_color: '#080808',
         display: 'standalone',
         icons: [
           {
@@ -30,6 +30,18 @@ export default defineConfig({
             src: '/icon-512.png',
             sizes: '512x512',
             type: 'image/png'
+          },
+          {
+            src: '/splash/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/splash/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
